eef9d594dbb5fc7c03dc596371a0e8be96862b60
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-10 14:09:58 +0000

    correcion

--- src/models.py
+++ src/models.py
@@ -1,82 +1,41 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Date
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    user_name = Column(String(30), unique=True)
-    bio = Column(String(120))
-    post = relationship('Post')
-    comment = relationship('Comments')
-    like = relationship('Likes')
-
-class Followers(Base):
-    __tablename__= 'followers'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    user_name = Column(String(30), unique=True)
-    bio = Column(String(120))
-
-class User_Follower(Base):
-    __tablename__ = 'user_follower'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user_id_relationship = relationship(User)
-    follower_id = Column(Integer, ForeignKey('followers.id'))
-    follower_id_relationship = relationship(Followers)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String)
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    description = Column(String(150))
-    publication_date = Column(Date)
-    user = Column(Integer, ForeignKey('user.id'))
-    media = relationship('Media')
-    comment = relationship('Comments')
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    comments = relationship('Comment')
+
+class Comment(Base):
+    __tablename__ = 'comentario'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String)
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    source = Column(String(255))
-    post = Column(Integer, ForeignKey('post.id'))
+    type_ = Column(Enum('image', 'video'))
+    uri = Column(String)
+    post_id = Column(Integer, ForeignKey('post.id'))
 
-class Comments(Base):
-    __tablename__ = 'comments'
-    id = Column(Integer, primary_key=True)
-    content = Column(String(150))
-    post = Column(Integer, ForeignKey('post.id'))
-    user = Column(Integer, ForeignKey('user.id'))
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_id = Column(Integer, ForeignKey('usuario.id'), primary_key=True)
+    follower_id = Column(Integer, ForeignKey('usuario.id'), primary_key=True)
 
-class Likes(Base):
-    __tablename__ = 'likes'
-    id = Column(Integer, primary_key=True)
-    date = Column(Date)
-    user = Column(Integer, ForeignKey('user.id'))
-
-class Likes_Posts(Base):
-    __tablename__ = 'likes_posts'
-    id = Column(Integer, primary_key=True)
-    like = Column(Integer, ForeignKey('likes.id'))
-    like_relationship = relationship(Likes) 
-    post = Column(Integer, ForeignKey('post.id'))
-    post_relationship = relationship(Post)
-
-
-
-    def to_dict(self):
-        return {}
-
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
